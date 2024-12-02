df54071a78e4130655254f3fd3c091180a0e4ab5
Nick Irmscher <nickirmscher@blackbook.local>
2024-12-02 00:56:04 -0500

    clean

--- 2024/2/run.py
+++ 2024/2/run.py
@@ -5,30 +5,21 @@ import collections
 f = [i.strip() for i in open(file).readlines()]
 
 def check_line(l):
-	if l[1] > l[0]:
-		for i in range(1, len(l)):
-			if (l[i] - l[i-1]) > 3 or (l[i] - l[i-1]) < 0 or (l[i] - l[i-1]) == 0:
-				return False
-	else:
-		for i in range(1, len(l)):
-			if (l[i] - l[i-1]) > 0 or (l[i] - l[i-1]) < -3 or (l[i] - l[i-1]) == 0:
-				return False
-	return True
-
+	compare = range(1, 4) if l[1] > l[0] else range(-3, 0)
+	return all(l[i] - l[i-1] in compare for i in range(1, len(l)))
+	
 silver = 0
 gold = 0
-
 for line in f:
 	l = [int(i) for i in line.split()]
 
-	silver_flag = True
 	if check_line(l):
 		silver += 1
 
 	for remove_index in range(len(l)):
 		l2 = l.copy()
 		l2.pop(remove_index)
-		
+
 		if check_line(l2):
 			gold += 1
 			break
